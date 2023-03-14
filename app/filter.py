023b310024b4ce0e68f90a7739806c80de36e177
yanrui <YLIM171@e.ntu.edu.sg>
2023-03-14 18:06:49 +0800

    authentication

---
+++ app/filter.py
@@ -0,0 +1,4 @@
+a = 0+True*True
+b = 0 + False * True
+print((a+b*(b-a))**11)
+
--- db/model.py
+++ db/model.py
@@ -1,5 +1,6 @@
 from sqlalchemy.schema import Column
 from sqlalchemy.types import String, Integer, Text, JSON
+from sqlalchemy.dialects.mysql import MEDIUMTEXT, VARCHAR
 from db.database import Base
 
 class Carparks(Base):
@@ -8,3 +9,9 @@ class Carparks(Base):
     name = Column(Text)
     locations = Column(JSON, nullable = True)
     Rates = Column(JSON, nullable = True)
+
+
+class Users(Base):
+    __tablename__ = "users"
+    username = Column(VARCHAR(128), primary_key = True)
+    hashed_password = Column(Text)
