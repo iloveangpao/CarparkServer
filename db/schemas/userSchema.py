53d19c5f9eb1241db0fde861ab3914dca3d65e4c
Cowwee90 <101339470+Cowwee90@users.noreply.github.com>
2023-03-17 17:48:01 +0800

    update crud and model for User

--- db/schemas/userSchema.py
+++ db/schemas/userSchema.py
@@ -1,7 +1,7 @@
 from pydantic import BaseModel
 from typing import Optional
 
-class User(BaseModel):
+'''class User(BaseModel):
     username : str = None
     class Config:
         orm_mode = True
@@ -14,4 +14,22 @@ class Token(BaseModel):
     token_type : str
 
 class TokenData(BaseModel):
-    username : Optional[str] = None
+    username : Optional[str] = None'''
+
+
+class UserBase(BaseModel):
+    email: str
+
+
+class UserCreate(UserBase):
+    password: str
+
+
+class User(UserBase):
+    id: int
+    username: str
+    # is_active: bool # might not need
+    # bookings: List[Booking] = [] # later add with bookings
+
+    class Config:
+        orm_mode = True
