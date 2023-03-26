4ae5f3ac8ac6d108e9b1c468dbbe0fab0a085772
Cowwee90 <101339470+Cowwee90@users.noreply.github.com>
2023-03-27 00:14:33 +0800

    validation checks for login

--- db/model.py
+++ db/model.py
@@ -27,7 +27,7 @@ class Carparks(Base):
 class User(Base):
     __tablename__ = "users"
 
-    id = Column(Integer, primary_key=True, index=True, autoincrement=True)
+    id = Column(Integer, primary_key=True, index=True)
     username = Column(String(50), unique=True, index=True)
     email = Column(String(50), unique=True, index=True)
     # phone = Column(String, unique=True, index=True)
