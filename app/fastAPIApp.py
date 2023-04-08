17b8fd017af3769e6adb84df4206c0c747a13c43
yanrui <YLIM171@e.ntu.edu.sg>
2023-04-08 22:30:52 +0800

    fix

--- app/fastAPIApp.py
+++ app/fastAPIApp.py
@@ -299,9 +299,9 @@ def read_carparks(skip: int = 0, limit: int = -1, db: Session = Depends(get_data
     return carparks
 
 @app.get('/carparkByCode/{code}', response_model=Carpark)
-def read_exact_carpark(skip: int = 0, limit: int = -1, db: Session = Depends(get_database_session, code):
+def read_exact_carpark(db: Session = Depends(get_database_session, code)):
     return crud.get_carpark_by_code(db,code)
-    
+
 @app.get("/avail/")
 async def getAvailCP():
     return URA().getAvailFinal()
